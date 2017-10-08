76cbe71bac3415ad5ccd61d87ebc4150e474e11f
Kyuyoung Shim <kshim3@myseneca.ca>
2017-10-08 16:34:03 -0400

    add

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 /*********************************************************************************
-* WEB322 – Assignment 02
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
-* of this assignment has been copied manually or electronically from any other source
-* (including 3rd party web sites) or distributed to other students.
+* WEB322 – Assignment 03
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
+* assignment has been copied manually or electronically from any other source (including web sites) or
+* distributed to other students.
 *
-* Name: Kyuyoung Shim   Student ID: 102562162   Date: 2017-09-18
+* Name: Kyuyoung Shim   Student ID: 102562162   Date: 2017-10-08
 *
 * Online (Heroku) Link:  https://immense-hamlet-93628.herokuapp.com/
+*
 ********************************************************************************/ 
-
+var dataService = require("./data-service.js");
 var express = require("express");
 var app = express();
 var path = require("path");
-var HTTP_PORT = process.env.PORT || 8080;
+var HTTP_PORT = process.env.port || 8080;
 
 function onHttpStart(){
     console.log("Express http server listening on " + HTTP_PORT);
 }
-app.use(express.static('public'));
 
 app.get("/", function(req,res){
     res.sendFile(path.join(__dirname + "/views/home.html"));
@@ -27,4 +27,74 @@ app.get("/about", function(req,res){
     res.sendFile(path.join(__dirname + "/views/about.html"));
 });
 
-app.listen(HTTP_PORT, onHttpStart);
+app.get("/employees", (req, res) => {
+    if (req.query.status == "Full Time" || req.query.status == "Part Time") {
+        var stat = req.query.status;
+        dataService.getEmployeesByStatus(stat).then((data) => {
+          res.json(data);
+        }).catch(() => {
+          res.json({ message: "no results returned" });
+        });
+    }
+      else if (req.query.manager) {
+        var mng = req.query.manager;
+        dataService.getEmployeesByManager(mng).then((data) => {
+          res.json(data);
+        }).catch(() => {
+          res.json({ message: "no results returned" });
+        });
+      }
+      else if (req.query.department) {
+        var depart = req.query.department;
+        dataService.getEmployeesByDepartment(depart).then((data) => {
+          res.json(data);
+        }).catch(() => {
+          res.json({ message: "no results returned" });
+        });
+      }
+      else {
+        dataService.getAllEmployees().then((data) => {
+              res.json(data);
+            }).catch(() => {
+              res.json({ message: "no results returned" });
+            });
+        }
+    });
+
+    app.get("/employee/:empNum", (req, res) => {
+        var num = req.params.empNum;
+        dataService.getEmployeeByNum(num).then((data) => {
+            res.json(data);
+        }).catch(() => {
+            res.json({ message: "no results returned" });
+        });
+    });
+    
+    app.get("/managers", (req, res) => {
+        dataService.getManagers().then((data) => {
+            res.json(data);
+        }).catch(() => {
+            res.json({ message: "no results returned" });
+        });
+    });
+
+    app.get("/departments", (req, res) => {
+        dataService.getDepartments().then((data) => {
+            res.json(data);
+        }).catch(() => {
+            res.json({ message: "no results returned" });
+        });
+    });
+
+    app.use((req, res) => {
+        res.status(404).send("Page Not Found");
+    });
+
+    dataService.initialize().then(() => {
+        app.listen(HTTP_PORT, onHttpStart);
+    }).catch((err) => {
+        console.log("Error: " + err);
+    });
+
+    app.listen(HTTP_PORT, onHttpStart);
+    app.use(express.static('public'));
