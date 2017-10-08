d44e68e5a62edbfdcc0273531048e15a4d87f4c4
Kyuyoung Shim <kshim3@myseneca.ca>
2017-10-08 16:49:02 -0400

    add

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var dataService = require("./data-service.js");
 var express = require("express");
 var app = express();
 var path = require("path");
-var HTTP_PORT = process.env.port || 8080;
+var HTTP_PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 
