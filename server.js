1229f3df8a9535b7dfd1d2dd7a5fb969ede0a785
Kyuyoung Shim <kshim3@myseneca.ca>
2017-10-22 19:41:25 -0400

    add

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 /*********************************************************************************
-* WEB322 – Assignment 03
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
-* assignment has been copied manually or electronically from any other source (including web sites) or
-* distributed to other students.
 *
+*  WEB322 – Assignment 04 
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this 
+*  assignment has been copied manually or electronically from any other source (including web sites) or  
+*  distributed to other students. 
+*  
 * Name: Kyuyoung Shim   Student ID: 102562162   Date: 2017-10-21
-*
-* Online (Heroku) Link:  https://immense-hamlet-93628.herokuapp.com/
-*
+* 
+*  Online (Heroku) Link: https://immense-hamlet-93628.herokuapp.com/
+* 
 ********************************************************************************/ 
 var dataService = require("./data-service.js");
 const exphbs = require('express-handlebars');      
@@ -52,61 +53,79 @@ app.get("/employees", (req, res) => {
     if (req.query.status == "Full Time" || req.query.status == "Part Time") {
         var stat = req.query.status;
         dataService.getEmployeesByStatus(stat).then((data) => {
-          res.json(data);
+            res.render("employeeList", { data: data, title: "Employees" });
         }).catch(() => {
-          res.json({ message: "no results returned" });
+            res.render("employeeList", { data: {}, title: "Employees" });
         });
     }
       else if (req.query.manager) {
         var mng = req.query.manager;
         dataService.getEmployeesByManager(mng).then((data) => {
-          res.json(data);
+            res.render("employeeList", { data: data, title: "Employees" });
         }).catch(() => {
-          res.json({ message: "no results returned" });
+            res.render("employeeList", { data: {}, title: "Employees" });
         });
       }
       else if (req.query.department) {
         var depart = req.query.department;
         dataService.getEmployeesByDepartment(depart).then((data) => {
-          res.json(data);
+            res.render("employeeList", { data: data, title: "Employees" });
         }).catch(() => {
-          res.json({ message: "no results returned" });
+            res.render("employeeList", { data: data, title: "Employees" });
         });
       }
       else {
         dataService.getAllEmployees().then((data) => {
-              res.json(data);
-            }).catch(() => {
-              res.json({ message: "no results returned" });
-            });
-        }
-    });
+            res.render("employeeList", { data: data, title: "Employees" });
+        }).catch(() => {
+            res.render("employeeList", { data: {}, title: "Employees" });
+        });
+    }
+});
 
     app.get("/employee/:empNum", (req, res) => {
         var num = req.params.empNum;
         dataService.getEmployeeByNum(num).then((data) => {
-            res.json(data);
+            res.render("employee", { data: data, title: "Employees" });
         }).catch(() => {
-            res.json({ message: "no results returned" });
+            res.status(404).send("Employee Not Found");
         });
     });
     
     app.get("/managers", (req, res) => {
         dataService.getManagers().then((data) => {
-            res.json(data);
+            res.render("employeeList", { data: data, title: "Employees (Managers)" });
         }).catch(() => {
-            res.json({ message: "no results returned" });
+            res.render("employeeList", { data: {}, title: "Employees (Managers)" });
         });
     });
 
     app.get("/departments", (req, res) => {
         dataService.getDepartments().then((data) => {
-            res.json(data);
+            res.render("departmentList", { data: data, title: "Departments" });
         }).catch(() => {
-            res.json({ message: "no results returned" });
+            res.render("departmentList", { data: {}, title: "Departments" });
         });
     });
 
+    app.get("/employees/add", (req, res) => {
+        res.render("addEmployee");
+      });
+      app.post("/employees/add", (req, res) => {
+        dataService.addEmployee(req.body).then((data)=>{
+        res.redirect("/employees");
+        });
+      });
+      app.post("/employee/update", (req, res) => 
+      { 
+        dataService.updateEmployee(req.body).then((data)=>{
+        res.redirect("/employees");
+        }); 
+    });
+
+
+
+    
     app.use((req, res) => {
         res.status(404).send("Page Not Found");
     });
