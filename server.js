d63a517d618de98418e677019977fb8669d3aa0b
Kyuyoung Shim <kshim3@myseneca.ca>
2017-12-10 14:26:51 -0500

    add

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /*********************************************************************************
 *
 *  WEB322 – Assignment 05 
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this 
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this 
 *  assignment has been copied manually or electronically from any other source (including web sites) or  
 *  distributed to other students. 
 *  
